b5f664acc1e064c84c73673a42f9216d8fa146e6
TmrSegev <tmr.segev@gmail.com>
2022-12-08 15:01:09 +0200

    figures unknown

--- ‏‏‏‏intersectionsTable.py
+++ ‏‏‏‏intersectionsTable.py
@@ -419,27 +419,56 @@ mask = unified.duplicated(subset=unified.columns.drop('Seed_mirGeneDB'), keep=Fa
 unified = unified.loc[~mask | (unified['Seed'] == unified['Seed_mirGeneDB'])]
 
 # ---Creating a families by type pivot table
-families_by_type = pd.pivot_table(unified, values='Description', index='Family', columns='Type', aggfunc='count')
-families_by_type.fillna(0, inplace=True)
-families_by_type.drop("UNKNOWN", axis=0, inplace=True)
-print(families_by_type)
-families_by_type.sort_values('Family')
-print(families_by_type)
-families_by_type.plot(kind='bar', figsize=(14, 10), stacked=True, title="{} counts of known families in each type".format(species))
-plt.ylabel("Counts")
-plt.legend(["1: Both", "2: miRDeep only", "3: sRNAbench only"])
-# plt.show()
-plt.savefig("{}_family_counts_per_type.png".format(species))
 
-# # for every type, create a bar chart of all the families
-# types = unified['Type'].unique()
-# for type in types:
-#     filtered_unified = unified[unified['Type'] == type]
-#     print(filtered_unified['Family'].value_counts())
-#     filtered_unified['Family'].value_counts().plot(kind='bar', figsize=(15, 10))
-#     plt.xticks(rotation=45, ha='right')
-#     plt.title("Counts by families for candidates of type " + str(type))
-#     plt.show()
+
+def families_by_type(df):
+    families_by_type = pd.pivot_table(df, values='Description', index='Family', columns='Type', aggfunc='count')
+    families_by_type.fillna(0, inplace=True)
+    families_by_type = families_by_type.drop("UNKNOWN", axis=0)
+    families_by_type.sort_values('Family', inplace=True)
+    families_by_type.plot(kind='bar', figsize=(14, 10), stacked=True, title="{} counts of known families in each type".format(species))
+    plt.ylabel("Counts")
+    plt.legend(["1: Both", "2: miRDeep only", "3: sRNAbench only"])
+    # plt.show()
+    plt.savefig("{}_family_counts_per_type.png".format(species))
+
+
+families_by_type(unified)
+
+# ---Creating unknown seeds by type pivot table
+
+
+def unknown_families_by_type(df):
+    filtered_df = df[df['Family'] == "UNKNOWN"]
+    pivot = pd.pivot_table(filtered_df, values='Description', index='Seed', columns='Type', aggfunc='count')
+    pivot.fillna(0, inplace=True)
+    pivot['sum'] = pivot.sum(axis=1)
+    pivot_groups = pivot[pivot['sum'] > 1]
+    pivot_solos = pivot[pivot['sum'] == 1]
+
+    pivot_groups = pivot_groups.drop('sum', axis=1)
+    pivot_groups.sort_values('Seed', inplace=True)
+    pivot_groups.plot(kind='bar', figsize=(14, 10), stacked=True,
+                          title="{} counts of unknown families in each type".format(species))
+    plt.ylabel("Counts")
+    plt.legend(["1: Both", "2: miRDeep only", "3: sRNAbench only"])
+    # plt.show()
+    plt.savefig("{}_unknown_family_counts_per_type.png".format(species))
+    plt.clf()
+
+    pivot_solos = pivot_solos.drop('sum', axis=1)
+    print(pivot_solos)
+    print(pivot_solos.sum())
+    pivot_solos.sum().plot(kind='pie', y='Type', autopct='%1.0f%%',
+                           title="{} unique seed candidates by type".format(species))
+    # plt.ylabel("Counts")
+    # plt.legend(["1: Both", "2: miRDeep only", "3: sRNAbench only"])
+    # plt.show()
+    plt.savefig("{}_unique_seed_candidates_by_type.png".format(species))
+
+
+
+unknown_families_by_type(unified)
 
 # -----Save each intersections table as a sheet in one excel file:-----
 
