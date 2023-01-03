1d4906de7cfa443096a85fa49b33ad03efb1c2a9
TmrSegev <tmr.segev@gmail.com>
2023-01-03 11:47:06 +0200

    new

---
+++ statistics.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+
+# ---Creating a families by type pivot table
+
+
+def families_by_type(df):
+    families_by_type = pd.pivot_table(df, values='Description', index='Family', columns='Type', aggfunc='count')
+    families_by_type.fillna(0, inplace=True)
+    families_by_type = families_by_type.drop("UNKNOWN", axis=0)
+    families_by_type.sort_values('Family', inplace=True)
+    families_by_type.plot(kind='bar', figsize=(14, 10), stacked=True, title="{} counts of known families in each type".format(species))
+    plt.ylabel("Counts")
+    # plt.legend(["1: Both", "2: miRDeep only", "3: sRNAbench only"])
+    # plt.show()
+    plt.savefig("{}_family_counts_per_type.png".format(species))
+
+
+families_by_type(unified)
+
+# ---Creating unknown seeds by type pivot table
+
+
+def unknown_families_by_type(df):
+    filtered_df = df[df['Family'] == "UNKNOWN"]
+    pivot = pd.pivot_table(filtered_df, values='Description', index='Seed', columns='Type', aggfunc='count')
+    pivot.fillna(0, inplace=True)
+    pivot['sum'] = pivot.sum(axis=1)
+    pivot_groups = pivot[pivot['sum'] > 1]
+    pivot_solos = pivot[pivot['sum'] == 1]
+
+    pivot_groups = pivot_groups.drop('sum', axis=1)
+    pivot_groups.sort_values('Seed', inplace=True)
+    pivot_groups.plot(kind='bar', figsize=(14, 10), stacked=True,
+                          title="{} counts of unknown families in each type".format(species))
+    plt.ylabel("Counts")
+    # plt.legend(["1: Both", "2: miRDeep only", "3: sRNAbench only"])
+    # plt.show()
+    plt.savefig("{}_unknown_family_counts_per_type.png".format(species))
+    plt.clf()
+
+    pivot_solos = pivot_solos.drop('sum', axis=1)
+    pivot_solos.sum().plot(kind='pie', y='Type', autopct='%1.0f%%',
+                           title="{} unique seed candidates by type".format(species))
+    plt.ylabel("Counts")
+    # plt.legend(["1: Both", "2: miRDeep only", "3: sRNAbench only"])
+    # plt.show()
+    plt.savefig("{}_unique_seed_candidates_by_type.png".format(species))
+
+
+if __name__ == '__main__':
+
+    unknown_families_by_type(unified)
