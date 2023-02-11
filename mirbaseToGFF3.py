5bafe9565c680aec00c68723f2ebfd0dabb18653
TmrSegev <tmr.segev@gmail.com>
2023-02-11 16:39:53 +0200

    new script

---
+++ mirbaseToGFF3.py
@@ -0,0 +1,6 @@
+# open gff file and fasta files
+# iterate rows
+# for every row in the gff, extract the name.
+# if it's precursor check the precursor file, if mature check the mature fasta file
+# look for the name in the fasta file.
+# take the corresponding sequence
